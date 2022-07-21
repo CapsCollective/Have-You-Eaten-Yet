ded3e6ebaa9b4476731f2e11be9c8036842a017c
Ben Kelly <benkellysoftware@gmail.com>
2022-07-21 19:38:31 +1000

    Dumpling Folding controls

--- Assets/Scripts/Wrapper.cs
+++ Assets/Scripts/Wrapper.cs
@@ -11,33 +11,57 @@ public class Wrapper : MonoBehaviour
     private SpriteRenderer _sprite;
     private DropTarget _dropTarget;
     private DragAndDrop _dragAndDrop;
+    private Collider2D _collider;
+    [SerializeField] private Collider2D leftFoldArea, rightFoldArea;
+
     private bool _folded;
     private bool _tilted;
     private bool _leftFolded;
     private bool _rightFolded;
-
-    [SerializeField] private int center;
-    private Vector2 StartPos = new Vector2(-10, 55);
-
     private void Start()
     {
         _sprite = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
         _dropTarget = GetComponent<DropTarget>();
         _dragAndDrop = GetComponent<DragAndDrop>();
+        _collider = GetComponent<Collider2D>();
         _dragAndDrop.enabled = false;
+    }
+    
+    private Vector2 MousePos => Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    [ReadOnly] public bool isSelected;
+    [ReadOnly] public float startY;
+
+    private void Update()
+    {
+        Vector2 mousePosition = MousePos;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (_collider.OverlapPoint(mousePosition))
+            {
+                isSelected = true;
+                startY = mousePosition.y;
+            }
+            if (rightFoldArea.OverlapPoint(mousePosition) && !_rightFolded) FoldRight();
+            if (leftFoldArea.OverlapPoint(mousePosition) && !_leftFolded) FoldLeft();
+        }
+
+        if (Input.GetMouseButtonUp(0)) isSelected = false;
+
+        if (!isSelected) return;
+        if (!_folded && mousePosition.y < startY - 6) FoldDown();
+        if (_folded && !_tilted && mousePosition.y > startY + 6) TiltUp();
     }
     
     public void Throw(Vector3 targetPos)
     {
         Transform t = transform;
-        t.position = StartPos;
         t.DOMove(targetPos, 1f);
         t.eulerAngles = new Vector3(0, 0, 180);
         t.DORotate(new Vector3(0, 0, 360), 1f);
     }
 
-    [Button]
     private void FoldDown()
     {
         if (!_dropTarget.dropped) return;
@@ -48,7 +72,6 @@ public class Wrapper : MonoBehaviour
         GetComponent<Collider2D>().isTrigger = false;
     }
     
-    [Button]
     private void TiltUp()
     {
         if (!_folded) return;
@@ -56,7 +79,6 @@ public class Wrapper : MonoBehaviour
         _tilted = true;
     }
     
-    [Button]
     private void FoldRight()
     {
         if (!_tilted) return;
@@ -64,7 +86,6 @@ public class Wrapper : MonoBehaviour
         _animator.Play("Fold Right");
     }
     
-    [Button]
     private void FoldLeft()
     {
         if (!_rightFolded) return;
